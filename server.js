64f63f895d83003c5d393cd8a7df9f6a2057b4ee
homiumusic-sketch <homiumusic@gmail.com>
2026-08-18 17:43:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,6 @@ app.use(express.static(__dirname));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
